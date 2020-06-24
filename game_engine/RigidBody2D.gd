4524ee539d493863cf6bb9f0dc6448dd5588a885
RoshanStacker <38171851+RoshanStacker@users.noreply.github.com>
2020-06-24 21:37:28 +1000

    Added Tween to decay of vel

--- game_engine/RigidBody2D.gd
+++ game_engine/RigidBody2D.gd
@@ -1,20 +1,30 @@
 extends RigidBody2D
 
-var velocity_accel = 500
+var minVel = 500
+var rateOfVelDecay = 200
+
+var velocity_accel = minVel
 var angular_accel = 300
+
+
 
 func _ready():
 	pass
 	
 
 func slowDown():
-	while(velocity_accel != 500):
-		velocity_accel -= 2
-		Tween
+	var velStart = velocity_accel
+	var decayDuration = (velStart - minVel) / rateOfVelDecay
+	$"../DecayTween".interpolate_property(self,"velocity_accel",velStart,minVel,decayDuration,Tween.TRANS_LINEAR)
+	$"../DecayTween".start()
 
 func _physics_process(delta):
+	print(velocity_accel)
 	if Input.is_action_pressed("up"):
 		#can try offset on blades with apply_impulse
+		if $"../DecayTween".is_active():
+			$"../DecayTween".stop_all()
+		
 		velocity_accel += 2
 		apply_central_impulse(Vector2(velocity_accel*delta*cos(deg2rad(rotation_degrees-90)),velocity_accel*delta*sin(deg2rad(rotation_degrees-90))))
 	if Input.is_action_just_released("up"):
