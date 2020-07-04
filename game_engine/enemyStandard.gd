cd7c6855fee0b86252e52445ef5d27330788e033
flynnlauridsen <flynn.lauridsen@students.mq.edu.au>
2020-07-04 21:50:49 +1000

    Core enemy movement is working
    They now move left or right randomly and increase movement speed when the player is near

--- game_engine/enemyStandard.gd
+++ game_engine/enemyStandard.gd
@@ -1,17 +1,35 @@
 extends RigidBody2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var moveDur = 5
+var moveDirection = 1
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 func _physics_process(delta):
+	global_translate(Vector2(moveDirection, 0))
+	moveDur -= delta
+	if($Area2D.overlaps_body($"/root/LevelAutoTileTest/Player") and abs(moveDirection) == 1):
+		moveDirection *= 3
+	elif(moveDur <= 0 and abs(moveDirection) == 3):
+		moveDirection = moveDirection / 3
+	if(moveDur <= 0):
+		moveDur = int(rand_range(5.0, 1.0))
+		moveDirection *= -1
+		$Sprite.flip_h = !$Sprite.flip_h
+		$CollisionPolygon2D.apply_scale(Vector2(-1, 1))
+	print(moveDirection)
+	if($Area2D.overlaps_body($"/root/LevelAutoTileTest/Player") and abs(moveDirection) == 1):
+		moveDirection *= 3
+	elif(moveDur <= 0 and abs(moveDirection) == 3):
+		moveDirection = moveDirection / 3
+		
+	
+		
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
-#	pass
+	#pass
